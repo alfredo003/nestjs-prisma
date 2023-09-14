ac42242f9d822ad97249ee906e6d586f32f181dc
Alfredo Manuel <alfredomanuel127.0@gmail.com>
2023-09-15 01:19:15 +0200

    created the migrations

---
+++ src/modules/books/books.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from 'src/database/prismaService';
+
+@Injectable()
+export class BooksService {
+  constructor(private prisma: PrismaService) { }
+
+  async create(data: CreateBookDto) {
+    const verifyBookExit = await this.prisma.book.findFirst({
+      where:{
+        bar_code:data.bar_code
+      }
+      })
+      if(verifyBookExit){
+        throw new Error('Book already exits');
+      }
+    const result = await this.prisma.book.create({
+      data
+    })
+
+    return result;
+  }
+
+  findAll() {
+    return `This action returns all books`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
---
+++ src/modules/books/dto/create-book.dto.ts
@@ -0,0 +1,7 @@
+export class CreateBookDto {
+  id?:string; 
+  title:string;
+  author:string;
+  description:string;
+  bar_code:string;
+}
